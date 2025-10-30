a79c040bb3d5998f1115680fd1a0cadeb9802de3
Trayan Iliev <office@iproduct.org>
2025-10-30 20:35:04 +0200

    medianBlur filter with gausian noise

--- fmi-2025-01-intro-opencv/cv_ex5.py
+++ fmi-2025-01-intro-opencv/cv_ex5.py
@@ -4,6 +4,11 @@ import numpy as np
 def nothing(x):
     pass
 
+def add_gaussian_noise(image, mean=0, std=5):
+        noise = np.random.normal(mean, std, image.shape).astype(np.uint8)
+        noisy_image = cv2.add(image, noise)
+        return noisy_image
+
 if __name__ == "__main__":
     # read a color image
     # cap = cv2.VideoCapture('test.mp4')
@@ -23,8 +28,10 @@ if __name__ == "__main__":
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             # identity_kernel = np.array([[0,0,0], [0,1,0], [0,0,0]])
             # identity_img = cv2.filter2D(src = frame_gray, ddepth=-1, kernel=identity_kernel)
-            gausian_blur = cv2.GaussianBlur(frame_gray, (11, 11), sigmaX = 16, sigmaY = 16)
-            cv2.imshow('video', gausian_blur)
+            # gausian_blur = cv2.GaussianBlur(frame_gray, (11, 11), sigmaX = 16, sigmaY = 16)
+            noisy_frame = add_gaussian_noise(frame_gray)
+            median_blur = cv2.medianBlur(frame_gray, ksize=5)
+            cv2.imshow('video', noisy_frame)
     else:
         print('Video opening failed')
 
