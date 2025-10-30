19d32e72731cad9fed9e999479f1fec1f0f9d073
Trayan Iliev <office@iproduct.org>
2025-10-30 20:41:49 +0200

    medianBlur filter with salt-and-paper noise

--- fmi-2025-01-intro-opencv/cv_ex5.py
+++ fmi-2025-01-intro-opencv/cv_ex5.py
@@ -9,6 +9,19 @@ def add_gaussian_noise(image, mean=0, std=5):
         noisy_image = cv2.add(image, noise)
         return noisy_image
 
+
+def add_salt_and_pepper_noise(image, noise_ratio=0.02):
+    noisy_image = image.copy()
+    h, w, c = noisy_image.shape
+    noisy_pixels = int(h * w * noise_ratio)
+    for _ in range(noisy_pixels):
+        row, col = np.random.randint(0, h), np.random.randint(0, w)
+        if np.random.rand() < 0.5:
+            noisy_image[row, col] = [0, 0, 0]
+        else:
+            noisy_image[row, col] = [255, 255, 255]
+    return noisy_image
+
 if __name__ == "__main__":
     # read a color image
     # cap = cv2.VideoCapture('test.mp4')
@@ -29,9 +42,10 @@ if __name__ == "__main__":
             # identity_kernel = np.array([[0,0,0], [0,1,0], [0,0,0]])
             # identity_img = cv2.filter2D(src = frame_gray, ddepth=-1, kernel=identity_kernel)
             # gausian_blur = cv2.GaussianBlur(frame_gray, (11, 11), sigmaX = 16, sigmaY = 16)
-            noisy_frame = add_gaussian_noise(frame_gray)
-            median_blur = cv2.medianBlur(frame_gray, ksize=5)
-            cv2.imshow('video', noisy_frame)
+            noisy_frame = add_salt_and_pepper_noise(frame)
+            median_blur = cv2.medianBlur(noisy_frame, ksize=3)
+            vis = np.hstack((noisy_frame, median_blur))
+            cv2.imshow('video', vis)
     else:
         print('Video opening failed')
 
