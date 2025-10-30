824efb4e85f687fb0c75f3e5d6288b4cd9b78261
Trayan Iliev <office@iproduct.org>
2025-10-30 21:50:46 +0200

    shape filtering

--- fmi-2025-01-intro-opencv/cv_ex7.py
+++ fmi-2025-01-intro-opencv/cv_ex7.py
@@ -1,42 +1,40 @@
 """
-cv_ex6.py - object tracking using color filtering
+cv_ex6.py - object tracking using color filtering and shapes recognition
 
 """
 
 import cv2
 import numpy as np
 
-def nothing(x):
-    pass
-
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
-    cv2.namedWindow("video")
-    cv2.createTrackbar("Min", "video", 105, 255, nothing)
-    cv2.createTrackbar("Max", "video", 140, 255, nothing)
-
     if(cap.isOpened()):
         print("webCam opened")
         while(cv2.waitKey(3) != ord('q')):
             ret, frame = cap.read()
-
-            # get min and max thresholds from trackbars
-            minThr = cv2.getTrackbarPos("Min", "video")
-            maxThr = cv2.getTrackbarPos("Max", "video")
-
             # convert color space from BGR to HSV
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             # set up lower and upper bound for color range of blue
-            lowBound = np.array([minThr,50,50])
-            upBound = np.array([maxThr,255,255])
+            lowBound = np.array([105,50,50])
+            upBound = np.array([130,255,255])
             # filtering by color
             mask = cv2.inRange(hsv,lowBound,upBound)
             # image dilation
             kernel = np.ones((5,5),np.uint8)
             mask = cv2.dilate(mask,kernel,iterations = 1)
+            # contour extraction
+            cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # show 2 objects (or more, depend on your setting)
+            cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:2] # 2
+            for c in cnts:
+                peri = cv2.arcLength(c,True)
+                approx = cv2.approxPolyDP(c, 0.02*peri, True)
+                if (len(approx)==4): # if it is 4-sides polygon
+                    cv2.drawContours(frame,[approx],0,(0,255,0),-1)
+                if (len(approx)>10): # if the polygon is more than 10 sides (may be circles)
+                    cv2.drawContours(frame,[approx],0,(0,0,255),-1)
+
             # combined two images (filter_image,original_image) in one window
-            # print(mask.shape)
-            # print(frame.shape)
             h1,w1 = mask.shape
             h2,w2,d2 = frame.shape
             imgCom = np.zeros((max([h1,h2]),w1+w2,3), dtype='uint8')
@@ -46,7 +44,7 @@ if __name__ == "__main__":
             imgCom[:h2,w1:w1+w2,:] = np.dstack([frame])
             # image resize
             # imgCom = cv2.resize(imgCom,(w1,h1),interpolation=cv2.INTER_LINEAR)
-            cv2.imshow("video",imgCom)
+            cv2.imshow("webCam",imgCom)
     else:
         print("Opening video failed")
     cap.release()
