30da366aa197b5c10039858a2708d131c7127fc8
Trayan Iliev <office@iproduct.org>
2025-11-27 19:56:06 +0200

    demos updated

--- fmi-2025-02-ml/decision_tree_simple.py
+++ fmi-2025-02-ml/decision_tree_simple.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn import tree
 iris = load_iris()
 X, y = iris.data, iris.target
-clf = tree.DecisionTreeClassifier()
+clf = tree.DecisionTreeClassifier(max_depth=2)
 clf = clf.fit(X, y)
 tree.plot_tree(clf)
 plt.show()
